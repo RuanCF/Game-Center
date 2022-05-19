6a58b05fb98bfb1373dd96447e52f7b05548d49b
Ruancf <darkcraft399@gmail.com>
2022-05-19 00:19:36 -0300

    NM

---
+++ ConnectDB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Game_Center
+{
+    class ConnectDB
+    {
+       public SQLiteConnection conn = new("Data Source=UserCenter.sdb");
+
+        public void Conectar()
+        {
+            conn.Open();
+        }
+
+        public void Desconectar()
+        {
+            conn.Close();
+        }
+    }
+}
+        
+
+    
+    
+   
+    
+
