6d33ccd7c399a736432f4b170e545bd7fb8c0b36
RuanCF <darkcraft399@gmail.com>
2022-05-19 12:44:39 -0300

    Add Song

---
+++ Song/ClassSong.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace Game_Center.Song
+{
+    public class ClassSong
+    {
+        public void PlaySong()
+        {
+            SoundPlayer Song = new(@"C:\\Users\\ruan-\\Documents\\C#\\Game-Center\\Song\\8Bit-Nostalgia.wav");
+            Song.Play();
+        }
+
+    }
+}
