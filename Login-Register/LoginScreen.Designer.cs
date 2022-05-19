c55e389f022d7b1c1d562e57d34f052adc63e3c2
Ruancf <darkcraft399@gmail.com>
2022-05-19 00:22:39 -0300

    NM

--- Login-Register/LoginScreen.Designer.cs
+++ Login-Register/LoginScreen.Designer.cs
@@ -29,24 +29,63 @@ namespace Game_Center.Screens
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.txtNick = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnLogin.Location = new System.Drawing.Point(229, 382);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(137, 40);
+            this.BtnLogin.TabIndex = 0;
+            this.BtnLogin.Text = "Login";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // txtNick
+            // 
+            this.txtNick.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtNick.Location = new System.Drawing.Point(210, 141);
+            this.txtNick.Name = "txtNick";
+            this.txtNick.PlaceholderText = "Nick";
+            this.txtNick.Size = new System.Drawing.Size(174, 35);
+            this.txtNick.TabIndex = 1;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtSenha.Location = new System.Drawing.Point(210, 236);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.PlaceholderText = "Senha";
+            this.txtSenha.Size = new System.Drawing.Size(174, 35);
+            this.txtSenha.TabIndex = 2;
             // 
             // LoginScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(600, 520);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtNick);
+            this.Controls.Add(this.BtnLogin);
             this.Name = "LoginScreen";
-            this.Sizable = false;
             this.Text = "LoginScreen";
             this.Load += new System.EventHandler(this.LoginScreen_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.LoginScreen_Paint_1);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
+
         #endregion
 
-
+        private System.Windows.Forms.Button BtnLogin;
+        private System.Windows.Forms.TextBox txtNick;
+        private System.Windows.Forms.TextBox txtSenha;
     }
 }
