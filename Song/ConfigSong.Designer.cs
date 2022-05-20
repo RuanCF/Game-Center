993a7471fbb2d43e215a3cd35d742ef0cfab1371
RuanCF <darkcraft399@gmail.com>
2022-05-20 09:56:22 -0300

    Create SongScreen

---
+++ Song/ConfigSong.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Game_Center.Song
+{
+    partial class ConfigSong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ConfigSong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "ConfigSong";
+            this.Text = "ConfigSong";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
