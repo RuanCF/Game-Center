a66e28570067cd4cbacc1a27e73fc8a3b3d663d6
Igao2 <igor.dasilvacosta8@gmail.com>
2022-05-30 16:49:24 -0300

    Pong game added

--- Games/Pong.Designer.cs
+++ Games/Pong.Designer.cs
@@ -28,34 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.cpuPlayer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuPlayer)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel1
+            // timer1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.panel1.Location = new System.Drawing.Point(55, 208);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(31, 125);
-            this.panel1.TabIndex = 0;
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // panel2
+            // player1
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.panel2.Location = new System.Drawing.Point(716, 208);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(31, 125);
-            this.panel2.TabIndex = 1;
+            this.player1.BackColor = System.Drawing.Color.White;
+            this.player1.Location = new System.Drawing.Point(54, 159);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(26, 146);
+            this.player1.TabIndex = 0;
+            this.player1.TabStop = false;
             // 
-            // panel3
+            // ball
             // 
-            this.panel3.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.panel3.Location = new System.Drawing.Point(407, 252);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(31, 31);
-            this.panel3.TabIndex = 1;
+            this.ball.BackColor = System.Drawing.Color.White;
+            this.ball.Location = new System.Drawing.Point(415, 176);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(26, 29);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // cpuPlayer
+            // 
+            this.cpuPlayer.BackColor = System.Drawing.Color.White;
+            this.cpuPlayer.Location = new System.Drawing.Point(786, 159);
+            this.cpuPlayer.Name = "cpuPlayer";
+            this.cpuPlayer.Size = new System.Drawing.Size(26, 146);
+            this.cpuPlayer.TabIndex = 2;
+            this.cpuPlayer.TabStop = false;
             // 
             // Pong
             // 
@@ -63,21 +77,26 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(856, 499);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.cpuPlayer);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.player1);
             this.Name = "Pong";
             this.Text = "Pong";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Pong_FormClosed);
             this.Load += new System.EventHandler(this.Pong_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Pong_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Pong_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpuPlayer)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox cpuPlayer;
     }
 }
